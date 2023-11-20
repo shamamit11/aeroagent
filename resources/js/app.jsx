011edf83eb795ffebe30914c2a3afbe32333cb1f
Amit Sharma <shamamit11@gmail.com>
2023-11-20 10:58:33 +0400

    fix

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,6 +1,7 @@
 import './bootstrap';
 import "../css/app.scss";
 
+import React from 'react'
 import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
@@ -13,8 +14,8 @@ createInertiaApp({
     resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
     setup({ el, App, props }) {
         const root = createRoot(el);
-
-        root.render(<App {...props} />);
+        //root.render(<App {...props} />);
+        root.render(React.createElement(App, props))
     },
     progress: {
         color: '#4B5563',
