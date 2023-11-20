8853c8bece099c4a6fb126393048a1cdb029fbbf
Amit Sharma <shamamit11@gmail.com>
2023-11-20 11:31:16 +0400

    fix

--- resources/views/app.blade copy.php
+++ resources/views/app.blade copy.php
@@ -15,7 +15,7 @@
     <!-- Scripts -->
     @routes
     @viteReactRefresh
-    @vite(['resources/js/app.jsx'])
+    @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
     @inertiaHead
 </head>
 
