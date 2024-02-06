8482265259853bcfc326fab8583a315f50ca36ba
Amit Sharma <shamamit11@gmail.com>
2024-02-06 10:44:24 +0545

    fix

--- app/Services/Agent/FeedService.php
+++ app/Services/Agent/FeedService.php
@@ -67,7 +67,7 @@ class FeedService
 
                     foreach ($current_arr_value as $key) {
                         $res = Location::where('id', $key)->first();
-                        array_push($locations, $res->name ? $res->name : '-');
+                        array_push($locations, @$res->name ? @$res->name : '-');
                     }
 
                     $location_array = implode(", ", $locations);
